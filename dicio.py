60c60f510b3086a57dff7f1c37738cea0f461c29
wnhirsch <wnhirsch@inf.ufrgs.br>
2018-06-20 02:56:31 -0300

    Fiz mais algumas modificações, mas praticamente nada novo. Algumas melhorias nas funções de simplificação de strings retirando agora a acentuação e melhorando na pontuação Também foi refeita a função que reduz para o radical, até agora ela apenas elimina os com 3 ou mais repetições

--- dicio.py
+++ dicio.py
@@ -1,3 +1,5 @@
+from unicodedata import normalize # Funcao que retorna o caractere normalizado
+
 ################################################################################
 # Classe Tweet que armazena o valor do sentimento do Tweet e o Tweet
 class Tweet():
@@ -20,7 +22,7 @@ def readCSVdicio(filename):
             # se for peso 0
             if(line[-2] is '0'):
                 # simplifica texto do tweet
-                line = reduceTT(line[:-2]) 
+                line = reduceTT(line[:-2])
                 # armazena no vetor
                 ttVector.append(Tweet(line,0))
             # se for peso -1
@@ -42,40 +44,47 @@ def reduceTT(tweet):
     # deixa todos os caracteres minusculos
     tweet = tweet.lower()
 
-    # retira as pontuacoes
+    # elimina as pontuacoes
     points = ['/','|','!','?','@','#','$','%','&','*',')','(','_','-','+','=','}',
          ']','[','{',':',';','.',',','>','<','^','~','\'','\"']
     reduced = ""
     for char in list(tweet):
         if char not in points:
             reduced += char
+        else:
+            reduced += ' '
+
+    # elimina os acentos
+    reduced = normalize('NFKD', reduced).encode('ASCII', 'ignore').decode('ASCII')
 
     # elimina todas as palavras com tamanho > 2
     words = reduced.split()
     reduced = ""
     for word in words:
+        word = reduce2radical(word)
         if len(word) > 2:
             # Adicionamos ao tweet reduzido, além das palavras com tamanho > 2,
             # o radical da propria palavra removendo letras desnecessarias
-            reduced += reduce2radical(word) + " "
+            reduced += word + " "
     # elimina o ultimo espaço
     reduced = reduced[:-1]
 
     return reduced
-    
+
 # Funcao que dado uma palavra a reduz para o seu radical, caso já esteja reduzida
 # nao a modifica
 def reduce2radical(word):
-    # Comecei e não terminei
-    # lastChar = ''
-    # reduced = ""
-    # for char in list(word):
-    #     if char != lastChar:
-    #         reduced += char
-    #     lastChar = char
-    # return reduced
-    return word
-    
-    
-    
-    
+    # elimina letras repetidas mais de 2 vezes na palavra
+    lastChar = ''
+    reduced = ""
+    count = 0
+    for char in list(word):
+        if char == lastChar and count < 2:
+            count += 1
+        elif char != lastChar:
+            count = 0
+        if not (char == lastChar and count == 2):
+            reduced += char
+        lastChar = char
+
+    return reduced
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from dicio import *
 
 ########################## MAIN DE TESTE
+
 filename = input("Digite o nome do arquivo: ")
 v = readCSVdicio(filename)
 
-print(len(v))
 for i in range(len(v)):
     print(v[i])
-    
